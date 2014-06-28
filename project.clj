6aa50f8c96f08a1294ab2879bbf5f719718b889e
Homer Strong <homer.strong@gmail.com>
2014-06-27 20:30:47 -0700

    commit project.clj

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject squint "0.1.0-SNAPSHOT"
+  :description "Converts images to ASCII art"
+  :url "https://github.com/strongh/squint"
+  :main squint.core
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [net.mikera/imagez "0.3.1"]
+                 [prismatic/hiphip "0.2.0"]])
